7ca8a2874244005fa4b46341b969d2b790a11f4e
Andres Hung <2018118240@ub.edu.bz>
2026-03-21 17:01:50 -0600

    feat: Add introduction

--- drone-color-detector/cmd/detector/main.go
+++ drone-color-detector/cmd/detector/main.go
@@ -2,11 +2,30 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"gocv.io/x/gocv"
 )
 
 func main() {
-	fmt.Printf("gocv version: %s\n", gocv.Version())
-	fmt.Printf("opencv lib version: %s\n", gocv.OpenCVVersion())
+	introduction()
+
+	log.Printf("gocv version: %s\n", gocv.Version())
+	log.Printf("opencv lib version: %s\n", gocv.OpenCVVersion())
+}
+
+// introduction displays project information and links.
+func introduction() {
+	const (
+		YELLOW = "\u001B[33m"
+		RESET  = "\u001B[0m"
+	)
+
+	fmt.Printf("%-35s %s\n", YELLOW+"[drone-color-detector]"+RESET,
+		"Program for communicating with the drone and detecting a color signal")
+	fmt.Printf("%-35s %s\n", YELLOW+"[GitHub Repository]"+RESET,
+		"https://github.com/andreshungbz/robot-maze-handshake")
+	fmt.Printf("%-35s %s\n", YELLOW+"[Project Subfolder]"+RESET,
+		"https://github.com/andreshungbz/robot-maze-handshake/tree/main/drone-color-detector")
+	fmt.Println()
 }
