6afa9e51c989730150160ebcd3209c76618d2587
Andres Hung <2018118240@ub.edu.bz>
2026-03-21 13:45:35 -0600

    feat: Add program introduction

--- robot-maze-solver/src/main.cpp
+++ robot-maze-solver/src/main.cpp
@@ -26,11 +26,14 @@ MazeSolver mazeSolver{motors, us, lfs};
 // Command Parser
 CommandParser parser{rgbLED, motors, currentMode};
 
+// forward declaration for introduction
+void introduction();
+
 void setup() {
     Serial.begin(9600); // debugging/monitoring
     ble.begin(115200); // Makeblock BLEV1.0_Z module
 
-    Serial.println("[robot-maze-solver] Starting in [MANUAL] mode...");
+    introduction();
 }
 
 void loop() {
@@ -63,4 +66,27 @@ void loop() {
     }
 
     delay(AUTONOMOUS_MAIN_LOOP_DELAY);
+}
+
+// introduction displays project information and links.
+void introduction() {
+    const char* PURPLE = "\x1B[95m";
+    const char* RESET  = "\x1B[0m";
+
+    Serial.print(PURPLE);
+    Serial.print("[robot-maze-solver]");
+    Serial.print(RESET);
+    Serial.println("Program that navigates a robot through a maze using the right-hand rule (or manually)");
+
+    Serial.print(PURPLE);
+    Serial.print("[GitHub Repository]");
+    Serial.print(RESET);
+    Serial.println(" https://github.com/andreshungbz/robot-maze-handshake");
+
+    Serial.print(PURPLE);
+    Serial.print("[Project Subfolder]");
+    Serial.print(RESET);
+    Serial.println(" https://github.com/andreshungbz/robot-maze-handshake/tree/main/robot-maze-solver");
+
+    Serial.println();
 }
