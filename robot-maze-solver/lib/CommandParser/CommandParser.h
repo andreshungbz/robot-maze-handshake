969f30d2337599c67e0d271dd70b53ec8c222811
Andres Hung <2018118240@ub.edu.bz>
2026-03-21 13:52:50 -0600

    docs: Document CommandParser

--- robot-maze-solver/lib/CommandParser/CommandParser.h
+++ robot-maze-solver/lib/CommandParser/CommandParser.h
@@ -4,15 +4,20 @@
 #include "RobotMode.h"
 #include "MotorController.h"
 
+// CommandParser parses and activates commands sent from the robot-bt-controller
+// program while the robot is in [MANUAL] mode.
 class CommandParser {
 public:
     // constructor
     CommandParser(RGBLEDController& led, MotorController& motors, RobotMode& mode)
         : rgbLED(led), motors(motors), currentMode(mode), RGBLEDOn(false) {}
 
+    // parseCommand examines commands and activates the appropriate module or motors.
     void parseCommand(char cmd);
 
 private:
+    // Data Members
+
     RGBLEDController& rgbLED;
     MotorController& motors;
     RobotMode& currentMode;
