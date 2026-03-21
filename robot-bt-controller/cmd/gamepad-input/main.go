504d86261dd84badfef688d2e38b217d5b4d73cf
Andres Hung <2018118240@ub.edu.bz>
2026-03-21 12:52:06 -0600

    feat: Add program introduction

--- robot-bt-controller/cmd/gamepad-input/main.go
+++ robot-bt-controller/cmd/gamepad-input/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/veandco/go-sdl2/sdl"
 )
 
 func main() {
+	introduction()
+
 	// initialize SDL library and controller subsystem
 	if err := sdl.Init(sdl.INIT_GAMECONTROLLER); err != nil {
 		log.Fatal(err)
@@ -14,27 +17,27 @@ func main() {
 	defer sdl.Quit()
 
 	if sdl.NumJoysticks() < 1 {
-		log.Fatal("No game controllers connected")
+		log.Fatal("[GAMEPAD-INPUT] No game controllers connected")
 	}
 
 	// open first controller
 	controller := sdl.GameControllerOpen(0)
 	if controller == nil {
-		log.Fatal("Failed to open game controller")
+		log.Fatal("[GAMEPAD-INPUT] Failed to open game controller")
 	}
 	defer controller.Close()
 
-	log.Printf("[MONITORING CONTROLLER -  %s]\n", controller.Name())
-	log.Printf("%-15s %-18s %-12s\n", "Input Type", "Control", "Value")
+	log.Printf("[GAMEPAD-INPUT] [MONITORING CONTROLLER -  %s]\n", controller.Name())
+	log.Printf("%-15s %-18s %-12s\n", "[INPUT TYPE]", "[CONTROL]", "[VALUE]")
 
 	for {
 		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
 			switch e := event.(type) {
 			// controller button events (e.g. d-pad, a, x)
 			case *sdl.ControllerButtonEvent:
-				state := "Released"
+				state := "released"
 				if e.State == sdl.PRESSED {
-					state = "Pressed"
+					state = "pressed"
 				}
 				log.Printf("%-15s %-18s %-12s\n",
 					"[Button]",
@@ -57,4 +60,19 @@ func main() {
 
 		sdl.Delay(20)
 	}
+}
+
+func introduction() {
+	const (
+		PURPLE = "\u001B[95m"
+		RESET  = "\u001B[0m"
+	)
+
+	fmt.Printf("%-35s %s\n", PURPLE+"[gamepad-input]"+RESET,
+		"Program for observing monitoring game controller input values")
+	fmt.Printf("%-35s %s\n", PURPLE+"[GitHub Repository]"+RESET,
+		"https://github.com/andreshungbz/robot-maze-handshake")
+	fmt.Printf("%-35s %s\n", PURPLE+"[Project Subfolder]"+RESET,
+		"https://github.com/andreshungbz/robot-maze-handshake/tree/main/robot-bt-controller")
+	fmt.Println()
 }
