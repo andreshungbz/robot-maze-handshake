f4cfc76a305cd780ac99ce5481eb8f3304c5a876
Andres Hung <2018118240@ub.edu.bz>
2026-04-23 17:31:05 -0600

    fix: Add NECESSARY waits in between commands so that drone doesn't go crazy

--- drone-color-detector/internal/drone/drone.go
+++ drone-color-detector/internal/drone/drone.go
@@ -185,15 +185,21 @@ func ExecuteSequence(d *Drone) {
 		return
 	}
 
+	time.Sleep(5 * time.Second)
+
 	if err := d.Flip("f"); err != nil {
 		log.Println(err)
 		return
 	}
 
-	if err := d.Forward(20); err != nil {
+	time.Sleep(5 * time.Second)
+
+	if err := d.Forward(250); err != nil {
 		log.Println(err)
 		return
 	}
+
+	time.Sleep(5 * time.Second)
 
 	if err := d.Land(); err != nil {
 		log.Println(err)
