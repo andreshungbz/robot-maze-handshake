ebdf816a84a79f202fdbe360a232f04ffe1b533c
Andres Hung <2018118240@ub.edu.bz>
2026-05-04 10:59:48 -0600

    misc: Change values

--- drone-color-detector/internal/drone/drone.go
+++ drone-color-detector/internal/drone/drone.go
@@ -185,21 +185,21 @@ func ExecuteSequence(d *Drone) {
 		return
 	}
 
-	time.Sleep(4 * time.Second)
+	time.Sleep(2 * time.Second)
 
 	if err := d.Flip("f"); err != nil {
 		log.Println(err)
 		return
 	}
 
-	time.Sleep(4 * time.Second)
+	time.Sleep(2 * time.Second)
 
-	if err := d.Forward(250); err != nil {
+	if err := d.Forward(175); err != nil {
 		log.Println(err)
 		return
 	}
 
-	time.Sleep(4 * time.Second)
+	time.Sleep(2 * time.Second)
 
 	if err := d.Land(); err != nil {
 		log.Println(err)
